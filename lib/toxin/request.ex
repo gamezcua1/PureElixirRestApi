cb7013c4e18da12298a2e57492815676657805f0
Gerald Amezcua <gamezcua1@hotmail.com>
2019-07-03 13:00:23 -0500

    [ADD] parsing header params and body

--- lib/toxin/request.ex
+++ lib/toxin/request.ex
@@ -1,33 +1,44 @@
 defmodule Toxin.Request do
   require Logger 
   
-  def parse({:error, _}) do
-    :error
-  end
+  def parse({:error, _}), do: :error
 
   def parse({:ok, request}) do
-    Logger.debug request
-    String.split(request, "\r\n\r\n")
-    |> parse_data
+    req = String.split(request, "\r\n\r\n")
+          |> parse_headers
+          |> parse_data
+          |> parse_body
 
+    IO.inspect req
     {:ok, Toxin.Response.get}
   end
 
-  defp parse_data([headers, data]) do
+  defp parse_headers([headers, data]) do
     [base |headers] = String.split(headers, "\r\n")
     [method, path, http_version] = String.split(base)
     req = %{"method" => :"#{method}", "path" => path, "http-version" => http_version }
+          |>add_header(headers)
 
-    req = add_header(headers, req)
-
-    IO.inspect req
+    [req, data]
   end
 
-  defp add_header([header |rest_headers], req) do
+  defp add_header(req, [header |rest_headers]) do
     [key, value] = String.downcase(header) |> String.split(": ")
-    add_header(rest_headers, Map.put(req, "#{key}", value))
+    add_header(Map.put(req, "#{key}", value), rest_headers)
   end
 
-  defp add_header([], req), do: req
+  defp add_header(req, []), do: req
+
+  defp parse_data([req, data]) do
+    params = get_params(req)
+    [Map.put(req, "params", params), data]
+  end
+
+  defp get_params(req) do
+    [_, params] = String.split(req["path"], "?") 
+    URI.decode_query(params)
+  end
+
+  defp parse_body([req, data]), do: Map.put(req, "body", Jason.decode!(data))
 
 end
